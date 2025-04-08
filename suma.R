fada71432afcb4f5cd2951cdc0a9de41c4225876
JorgeUF <jousuga@unal.edu.co>
2025-04-08 10:15:49 -0500

    suma

---
+++ suma.R
@@ -0,0 +1,27 @@
+y <- 1:10
+for (f in y ) {
+  print(f)
+}
+5:-5
+for (i in 5:-5) {
+  print(i)
+  }
+ u <- "hola"
+ for (r in u) {
+   print(r)
+1:6
+for(g in 1:6){
+  cat (g^2," ")
+ for (g in 1+4+9+16+25+36){
+   print (g)
+   {
+n<- 1:6
+b<- b + n **2
+   for(b in n)
+ print (n)
+
+
+}
+
+  
+   
